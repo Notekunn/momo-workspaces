525a3656708b940d3fd06fbe60dac582a9fa809a
DC <cuong.nl5.kma@gmail.com>
2022-08-07 16:22:17 +0700

    feat: add release github actions

--- lint-staged.js
+++ lint-staged.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.+(js|json|ts|tsx)': ['yarn lint:fix', 'yarn lint'],
-  '*.{js,jsx,ts,tsx,json,css,scss,md}': ['yarn format:write', 'yarn format:check'],
+  './libraries/**/*.+(js|json|ts|tsx)': ['yarn lint:fix', 'yarn lint'],
+  './libraries/**/*.{js,jsx,ts,tsx,json,css,scss,md}': ['yarn format:write', 'yarn format:check'],
 }
